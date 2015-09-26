80efd6b55c7d38c2699ffb5c42ad84c02f3457ee
Patrick Keenan <pgk4526@gmail.com>
2015-09-26 19:17:13 -0400

    fixed index.html.erb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,17 +11,17 @@ def index
     if a.low_res_url.nil?
       puts 'KAPPPPPPPAAA!!!!!'
       job = AzureMediaService::Job.get(a.job_id)
-        if job.State == '3'
+      puts job
+        if job.State == 3
           asset = job.output_assets()[0]
           policy = AzureMediaService::AccessPolicy.create(name: asset.Name,duration_minutes: 300, permission: 1)
           locator = AzureMediaService::Locator.create(policy_id: policy.Id, asset_id: asset.Id, type: 1)
           asset.files
           low_url = asset.Uri+"/"+asset.files[0].Name+locator.ContentAccessComponent
-          vid = Video.where(id: a.id)
-          vid.low_res_url = low_url
+          a.low_res_url = low_url
           job = asset.encode_job('H264 Adaptive Bitrate MP4 Set SD 4x3 for iOS Cellular Only')
-          vid.job_id = job.__metadata["id"]
-          vid.save
+          a.job_id = job.__metadata["id"]
+          a.save
 
       else
         puts "job not finished"
